9026fd1ed9169ed99d50123203dd5b4bc37efd17
Mike Nelson <mike@mikeonrails.com>
2012-08-06 10:08:52 -0700

    make rake task simpler and use chunked deletion strategy for riak keys

--- lib/storehouse/adapter/riak.rb
+++ lib/storehouse/adapter/riak.rb
@@ -8,6 +8,7 @@ module Storehouse
         super
         @bucket_name = options[:bucket] || ['_page_cache', Storehouse.config.scope].compact.join('_')
         @client_options = options[:client] || {}
+        @clearing_delta = options[:clearing_delta] || 1.week
       end
 
       protected
@@ -42,6 +43,7 @@ module Storehouse
         if expiration = expires_at(options)
           object.indexes['expires_at_int'] = expiration.to_i
         end
+        object.indexes['created_at_int'] = Time.now.to_i
 
         object.store
       
@@ -52,7 +54,30 @@ module Storehouse
       end
 
       def clear!(pattern = nil)
-        bucket.delete(pattern)
+        chunked_delete('created_at_int', 1.year.ago)
+      end
+
+      def clear_expired!
+        chunked_delete('expired_at_int', 1.year.ago)
+      end
+
+      def chunked_delete(index_name, start_time, end_time = nil)
+        t = end_time || Time.now
+        t0 = start_time
+
+        # chunked expiration. 1 @clearing_delta timespan for each key retrieval
+        begin
+
+          t1 = t0 + @clearing_delta
+          t1 = [t1, t].min
+
+          bucket.get_index(index_name, t0.to_i...t1.to_i).each do |k|
+            bucket.delete(k)
+          end
+
+          t0 += @clearing_delta
+
+        end while(t0 < t)
       end
 
     end
--- lib/storehouse/tasks.rb
+++ lib/storehouse/tasks.rb
@@ -3,37 +3,9 @@ namespace :storehouse do
     Storehouse.clear!
   end
 
-
-  namespace :riak do
-
-    task :clear_expired => :environment do 
-
-      raise "Storehouse not configured with Riak Adapter" unless Storehouse.config.adapter == 'Riak'
-
-      t = Time.now.to_i
-
-      adapter = Storehouse.send(:data_store)
-      bucket = adapter.send(:bucket)
-
-      t0 = 1.year.ago
-
-      # chunked expiration. 1 month timespan for each key retrieval
-      begin
-
-        t1 = t0 + 1.month
-        t1 = [t1, t].min
-
-        bucket.get_index('expires_at_int', t0.to_i...t1.to_i).each do |k|
-          bucket.delete(k)
-        end
-
-        t0 += 1.month
-
-      end while(t0 < t)
-
-
-    end
-
+  task :clear_expired => :environment do
+    store = Storehouse.send(:data_store)
+    store.send(:clear_expired!) if store.respond_to?(:clear_expired!)
   end
 
 end
