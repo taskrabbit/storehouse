e8c7698fb476a24731dc83ae4e2b750da6575b4e
Mike Nelson <mike@mikeonrails.com>
2012-08-28 11:01:05 -0700

    fix parameter count for controller

--- lib/storehouse/controller.rb
+++ lib/storehouse/controller.rb
@@ -35,7 +35,7 @@ module Storehouse
       super
     end
 
-    def cache_page(content, path, extension = nil, gzip = nil)
+    def cache_page(content, path, extension = nil)
       
       options = self.storehouse_page_cache_action && self.storehouse_page_cache_options.try(:[], self.storehouse_page_cache_action) || {}
 
