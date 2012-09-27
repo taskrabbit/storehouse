e6671d2c49bd6561c02d3dfa6343478005c33066
Mike Nelson <mike@mikeonrails.com>
2012-09-27 14:02:55 -0700

    break db dependency

--- spec/tester/config/application.rb
+++ spec/tester/config/application.rb
@@ -1,6 +1,6 @@
 require File.expand_path('../boot', __FILE__)
 
-require 'rails/all'
+require "action_controller/railtie"
 
 if defined?(Bundler)
   # If you precompile assets before deploying to production, use this line
@@ -51,12 +51,12 @@ module Tester
     # This will create an empty whitelist of attributes available for mass-assignment for all models
     # in your app. As such, your models will need to explicitly whitelist or blacklist accessible
     # parameters by using an attr_accessible or attr_protected declaration.
-    config.active_record.whitelist_attributes = true
+    # config.active_record.whitelist_attributes = true
 
     # Enable the asset pipeline
-    config.assets.enabled = true
+    # config.assets.enabled = true
 
     # Version of your assets, change this if you want to expire all your assets
-    config.assets.version = '1.0'
+    # config.assets.version = '1.0'
   end
 end
